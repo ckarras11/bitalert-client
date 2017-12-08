5369cc19dc62338e6417376f0a61bd81c2ef5fe4
Christopher Karras <ckarras11@gmail.com>
2017-12-07 19:17:49 -0500

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Logo from './logo';
+import Button from './button';
+import Refresh from './refresh';
+import PhoneNumber from './phone-number'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Logo />
+        <Button buttonMessage={'Set New Alert'} buttonType={'info button'} />
+        <Button buttonMessage={'Edit Alerts'} buttonType={'danger button'} />
+        <Button buttonMessage={'Submit'} buttonType={'success button'} />
+        <Refresh />
+        <PhoneNumber />
+      </div>
+    );
+  }
+}
+
+export default App;
