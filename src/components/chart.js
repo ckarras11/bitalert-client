0e298e5d092f84fe35d56b306e6daf2aee5e701a
Christopher Karras <ckarras11@gmail.com>
2017-12-07 20:20:18 -0500

    Added a chart component

---
+++ src/components/chart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './chart.css';
+
+class Chart extends Component {
+    render() {
+        return (
+            <div className='chart'>
+                <p>Chart</p>
+            </div>
+        );
+    }
+}
+
+export default Chart;
