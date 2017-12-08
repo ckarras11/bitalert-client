029b5912f42c3d1a01f41d6c7ca0f24c5e3124f4
Christopher Karras <ckarras11@gmail.com>
2017-12-07 19:18:09 -0500

    Added button component

---
+++ src/components/button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './button.css'
+
+class Button extends Component {
+    render() {
+        return (
+            <div>
+                <button className={this.props.buttonType}>{this.props.buttonMessage}</button>
+            </div>
+        );
+    }
+}
+
+export default Button;
