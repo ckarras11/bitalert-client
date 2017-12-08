47de518d9d2ac5846ab15d8fea925a07648d9356
Christopher Karras <ckarras11@gmail.com>
2017-12-07 19:37:48 -0500

    Added footer component

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './footer.css'
+const Footer = () => {
+    return (
+        <footer>
+            <p>Alerts Must be set every 24 hours</p>
+        </footer>
+    );
+};
+
+export default Footer;
