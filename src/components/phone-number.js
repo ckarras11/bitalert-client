aa7b4bd560418a919c89662d952c9f1b42718c39
Christopher Karras <ckarras11@gmail.com>
2017-12-07 19:38:01 -0500

    Added phone number component

--- src/components/phone-number.js
+++ src/components/phone-number.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import './phone-number.css'
 class PhoneNumber extends Component {
     render() {
         return (
-            <div>
+            <div className='phone-number-container'>
                 <label>Phone Number</label>
                 <input type='tel' placeholder='(999)-999-9999'></input>
             </div>
