422b172af36dedad1220d3df0a8763f886658893
Christopher Karras <ckarras11@gmail.com>
2017-12-07 19:17:13 -0500

    Added refresh component

---
+++ src/components/refresh.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Refresh extends Component {
+    render() {
+        return (
+            <div>
+                <button><i class="fa fa-refresh" aria-hidden="true"></i></button>
+            </div>
+        );
+    }
+}
+
+export default Refresh;
