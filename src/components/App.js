21306ea08893750070f9d272a49eb2aff080c316
Christopher Karras <ckarras11@gmail.com>
2017-12-07 20:21:07 -0500

    Imported more components

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Logo from './logo';
 import Button from './button';
 import Refresh from './refresh';
-import PhoneNumber from './phone-number'
-import Footer from './footer'
+import PhoneNumber from './phone-number';
+import Footer from './footer';
+import Price from './price';
+import Chart from './chart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Logo />
+        <Price />
+        <Chart />
         <Button buttonMessage={'Set New Alert'} buttonType={'info button'} />
         <Button buttonMessage={'Edit Alerts'} buttonType={'danger button'} />
         <Button buttonMessage={'Submit'} buttonType={'success button'} />
