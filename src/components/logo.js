164b0195a8d386747aaffa91b23eb766dce7b78e
Christopher Karras <ckarras11@gmail.com>
2017-12-07 19:18:19 -0500

    Added logo

---
+++ src/components/logo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './logo.css'
+
+export default function Logo() {
+    return(
+        <img className='logo' src={require('../bitalert.png')} alt='bitalert logo'/>
+    )
+}
