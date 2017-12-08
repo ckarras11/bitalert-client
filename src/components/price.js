37eecc6d6b86bab654216646c0a087259912386b
Christopher Karras <ckarras11@gmail.com>
2017-12-07 20:20:54 -0500

    Added a price component

---
+++ src/components/price.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './price.css';
+
+class Price extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className='price'>BTC $17500.99</h1>
+            </div>
+        );
+    }
+}
+
+export default Price;
