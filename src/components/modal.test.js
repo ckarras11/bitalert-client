021ca33af44718b2c1610285c33e3b428ea5fab5
Christopher Karras <ckarras11@gmail.com>
2017-12-09 17:53:43 -0500

    Added test to modal.js

---
+++ src/components/modal.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow, mount} from 'enzyme';
+import Modal from './modal';
+
+describe('<Modal />', () => {
+    it('Renders without crashing', () => {
+        shallow(<Modal />);
+    })
+})
