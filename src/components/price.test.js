fbcbbf3f0b50eab048819032ba7b3e83d6d2c630
Christopher Karras <ckarras11@gmail.com>
2017-12-09 17:53:10 -0500

    Added test to price.js

---
+++ src/components/price.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {shallow, mount} from 'enzyme';
+import Price from './price';
+
+describe('<Price />', () => {
+    it('Renders without crashing', () => {
+        shallow(<Price />);
+    })
+})
