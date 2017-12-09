da3fbd63b1f6b3bbe56ebc60b2308acb46677ebe
Christopher Karras <ckarras11@gmail.com>
2017-12-09 17:56:40 -0500

    Added initial state and toggleModal action

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,42 @@
+import {
+    TOGGLE_MODAL,
+} from './actions'
+
+const initialState = {
+    showModal: false,
+    modalTitle: '',
+    alertPrice: 0,
+    modalType: ''
+
+}
+
 export default (state, action) => {
+    state = state || initialState;
+
+    if(action.type === TOGGLE_MODAL) {
+        if (action.modalContent === 'info') {
+            state = Object.assign({}, state, {
+                showModal: !state.showModal,
+                modalTitle: 'Set New Alert',
+                modalType: 'info'
+            });
+        } else if (action.modalContent === 'danger') {
+            state = Object.assign({}, state, {
+                showModal: !state.showModal,
+                modalTitle: 'Edit Alerts',
+                modalType: 'danger',
+            });
+        } else if (action.modalContent === 'success') {
+            state = Object.assign({}, state, {
+                showModal: !state.showModal,
+                alertPrice: action.alertPrice,
+            });
+        } else {
+            state = Object.assign({}, initialState, {
+            });
+        }
+        console.log(state)
+        return state;
+    }
     return state
 }
