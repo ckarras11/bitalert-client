1068ace339db648bf3b93812a7feec23c95b10f0
Christopher Karras <ckarras11@gmail.com>
2017-12-09 17:58:12 -0500

    Minor changes

--- src/components/phone-number.js
+++ src/components/phone-number.js
@@ -4,7 +4,7 @@ class PhoneNumber extends Component {
     render() {
         return (
             <div className='phone-number-container'>
-                <label>Phone Number</label>
+                <label>Phone Number:</label>
                 <input type='tel' placeholder='(999)-999-9999'></input>
             </div>
         );
