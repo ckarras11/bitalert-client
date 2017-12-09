61512158a16f676383d6d36c8c82128f798c3a5a
Christopher Karras <ckarras11@gmail.com>
2017-12-09 17:55:38 -0500

    Added fetch to get current price

--- src/components/price.js
+++ src/components/price.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import Refresh from './refresh';
 import './price.css';
 
 class Price extends Component {
+    componentDidMount() {
+        this.getPrice();
+    }
+    getPrice () {
+        /* fetch('https://api.coinbase.com/v2/prices/spot?currency=USD')
+            .then(res => {
+                console.log(res);
+            }) */
+            console.log('GET PRICE')
+    }
     render() {
         return (
-            <div>
+            <div className="price-container">
                 <h1 className='price'>BTC $17500.99</h1>
+                <Refresh />
             </div>
         );
     }
