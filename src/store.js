2fed7b09cabefe45be46c5a0d544166945835731
Christopher Karras <ckarras11@gmail.com>
2017-12-20 21:34:02 -0500

    Added thunkMiddleware to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,8 @@
-import {createStore} from 'redux';
-
+import thunkMiddleware from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
 import reducer from './reducer';
 
-export default createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+
+
+export default store
