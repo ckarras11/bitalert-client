36ab3832caa92f15add436a9a4558aeefa48e30c
Christopher Karras <ckarras11@gmail.com>
2017-12-21 20:52:13 -0500

    Minor changes

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './alert.css';
 
-const componentName = () => {
-    return (
-        <div className="alert-container">
-            <h1>Price</h1>
-        </div>
-    );
-};
 
-export default componentName;
+import { connect } from 'react-redux';
+import { fetchAlert } from '../actions';
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+export class Alert extends React.Component {
+    componentDidMount() {
+        this.props.dispatch(fetchAlert());
+    }
+    render() {
+        return (
+            <div className="alert-container">
+                <h1>Price</h1>
+            </div>
+        );
+    }
+}
+
+export default connect(
+    mapStateToProps,
+)(Alert);
