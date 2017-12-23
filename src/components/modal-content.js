0bc51c187ec64d248823743f6079014355c154b1
Christopher Karras <ckarras11@gmail.com>
2017-12-23 12:29:20 -0500

    Added a submit handler for the phone number input

--- src/components/modal-content.js
+++ src/components/modal-content.js
@@ -1,36 +1,50 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { fetchAlert } from '../actions';
 import PhoneNumber from './phone-number';
-import Button from './button';
 import Horizontal from './range-slider';
-import Alert from './alert'
+import AlertContainer from './alert-container';
+import Submit from './submit-button';
 
 function mapStateToProps(state) {
     return {
         modalTitle: state.modalTitle,
         modalType: state.modalType,
+        phoneNumber: state.phoneNumber
 
     };
 }
 
-export function ModalContent(props) {
-    let modalContent
-    if (props.modalType === 'info') {
-        modalContent = <Horizontal />
-    } else if (props.modalType === 'danger'){
-        modalContent = <Alert />
+export class ModalContent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.submitHandler = this.submitHandler.bind(this)
     }
+
+    submitHandler() {
+        this.props.dispatch(fetchAlert(this.props.phoneNumber));
+        
+    }
+
+    render() {
+        let modalContent
+        if (this.props.modalType === 'info') {
+            modalContent = <Horizontal />
+        } else if (this.props.modalType === 'danger') {
+            modalContent = <AlertContainer />
+        }
         return (
             <div className="modal-content">
-                <h2 className="modal-title">{props.modalTitle}</h2>
+                <h2 className="modal-title">{this.props.modalTitle}</h2>
                 <hr />
                 <PhoneNumber />
                 {modalContent}
-                <Button buttonMessage={'Submit'} buttonType={'success'} />
-                
+                <Submit action={this.submitHandler} />
             </div>
         );
     }
+
+}
 
 export default connect(
     mapStateToProps,
