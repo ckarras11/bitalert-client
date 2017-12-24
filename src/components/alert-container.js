2f460cb7b24433221317813d965d9f0bda0fb1cb
Christopher Karras <ckarras11@gmail.com>
2017-12-24 14:43:41 -0500

    Added title to alert container

--- src/components/alert-container.js
+++ src/components/alert-container.js
@@ -14,11 +14,12 @@ export class AlertContainer extends React.Component {
     
     render() {
         const userAlerts = this.props.alerts.map((alert, index) => {
-           return  <Alert key={index} price={alert.alert.price} />
+           return  <Alert key={index} number={alert.phoneNumber} price={alert.alert.price} alertId={alert.id}/> 
             
         })
         return (
             <div className="alert-container">
+                <h1 className="alert-title">Alerts</h1>
                 {userAlerts}
             </div>
         );
