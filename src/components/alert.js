1b46a72ff253685ee0c109223c9692370b4bcd42
Christopher Karras <ckarras11@gmail.com>
2017-12-24 14:43:23 -0500

    Added remove funcitonality to Alert

--- src/components/alert.js
+++ src/components/alert.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './alert.css';
+import { connect } from 'react-redux';
+import { removeAlert } from '../actions';
 
-export default function Alert(props) {
-    return (
-        <div className="alert">
-            <h1>{props.price}</h1>
-        </div>
-    );
+
+export class Alert extends React.Component {
+    handleRemove(e) {
+        e.preventDefault();
+        if(window.confirm('Are you sure you want to delete this alert?') === true) {
+            this.props.dispatch(removeAlert(e.currentTarget.id, this.props.number))
+        } 
+    }
+
+    render() {
+        return (
+            <div className="alert">
+                <h1 className="alert-price">{this.props.price}</h1>
+                <span id={this.props.alertId} number={this.props.number} onClick={e => this.handleRemove(e)} className="close">&times;</span>
+            </div>
+        );
+    }   
 };
+
+export default connect()(Alert);
