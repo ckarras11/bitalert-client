ff9417c2233cf25fac4633a9afb8997dd5e787b4
Christopher Karras <ckarras11@gmail.com>
2018-01-01 12:13:35 -0500

    Changed async actions to arrow functions

--- src/actions.js
+++ src/actions.js
@@ -7,6 +7,7 @@ export const toggleModal = (modalContent, alertPrice) => ({
     modalContent,
     alertPrice,
 });
+
 // Runs when there is a succesful call to the database to get the current price
 // Turns off the loading wheel
 export const FETCH_PRICE_SUCCESS = 'FETCH_PRICE_SUCCESS';
@@ -15,12 +16,14 @@ export const fetchPriceSuccess = res => ({
     res,
 
 });
+
 // Initiates the loading wheel while the database is being queried for the current price
 export const FETCH_PRICE_INITIATED = 'FETCH_PRICE_INITIATED';
 export const fetchPriceInitiated = () => ({
     type: FETCH_PRICE_INITIATED,
 
 });
+
 // Runs when there is a succesful call to the db for alerts
 export const FETCH_ALERT_SUCCESS = 'FETCH_ALERT_SUCCESS';
 export const fetchAlertSuccess = res => ({
@@ -28,81 +31,95 @@ export const fetchAlertSuccess = res => ({
     res,
 });
 
+// Triggered if alerts array fetched is empty
 export const FETCH_ALERT_EMPTY = 'FETCH_ALERT_EMPTY';
 export const fetchAlertEmpty = () => ({
     type: FETCH_ALERT_EMPTY,
 });
+
 // Sets the state with the phonenumber input
 export const SET_PHONENUMBER = 'SET_PHONENUMBER';
 export const setPhonenumber = phoneNumber => ({
     type: SET_PHONENUMBER,
     phoneNumber,
 });
+
 // Sets the state with the alert price from the range slider
 export const SET_ALERT_PRICE = 'SET_ALERT_PRICE';
 export const setAlertPrice = alertPrice => ({
     type: SET_ALERT_PRICE,
     alertPrice,
 });
+
 // Closes the modal and sets the phonenumber in state to ''
 export const CREATE_ALERT_SUCCESS = 'CREATE_ALERT_SUCCESS';
 export const createAlertSuccess = res => ({
     type: CREATE_ALERT_SUCCESS,
     res,
 });
+
 // Triggered from modal content to create a new alert
-export function createAlert(phoneNumber, alertPrice) {
-    return function (dispatch) {
-        return fetch(`${API_BASE_URL}/api/alerts`, {
- method: 'POST', mode: 'cors', body: JSON.stringify({ phoneNumber, alert: { price: alertPrice } }), headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-})
-            .then(res => res.json())
-            .then((json) => {
-                dispatch(createAlertSuccess(json));
-            });
-    };
+export const createAlert = (phoneNumber, alertPrice) => dispatch => {
+    return fetch(`${API_BASE_URL}/api/alerts`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({phoneNumber, alert: {price: alertPrice}})
+    })
+    .then(res => res.json())
+    .then(json => dispatch(createAlertSuccess(json)));
 }
+
 // Triggered form alert container to remove alert
-export function removeAlert(id, number) {
-    return function (dispatch) {
-        return fetch(`${API_BASE_URL}/api/alerts/${id}`, { method: 'DELETE', mode: 'cors', headers: { Accept: 'application/json' } })
-            .then((res) => {
-                dispatch(fetchAlert(number));
-            });
-    };
+export const removeAlert = (id, number) => dispatch => {
+    return fetch(`${API_BASE_URL}/api/alerts/${id}`, { 
+        method: 'DELETE', 
+        mode: 'cors', 
+        headers: { Accept: 'application/json' } 
+    })
+    .then(res => dispatch(fetchAlert(number)));
 }
+
 // Triggered when app loads and when refresh button is clicked
 // Gets current price
-export function fetchPrice() {
-    return function (dispatch) {
-        dispatch(fetchPriceInitiated());
-        return fetch(`${API_BASE_URL}/api/price`, { method: 'GET', mode: 'cors', headers: { Accept: 'application/json' } })
-            .then(res => res.json())
-            .then((json) => {
-                json.sort(function (a, b) {
-                    return new Date(a.timestamp) - new Date(b.timestamp);
-                });
-                dispatch(fetchPriceSuccess(json));
-            });
-    };
+export const fetchPrice = () => dispatch => {
+    dispatch(fetchPriceInitiated());
+    return fetch(`${API_BASE_URL}/api/price`, { 
+        method: 'GET', 
+        mode: 'cors', 
+        headers: { Accept: 'application/json' } 
+    })
+    .then(res => res.json())
+    .then(json => {
+        json.sort(function (a, b) {
+            return new Date(a.timestamp) - new Date(b.timestamp);
+        });
+        dispatch(fetchPriceSuccess(json));
+    });
 }
+
 // Trigged on submit when getting alerts for a phonenumber from modal content
-export function fetchAlert(phoneNumber) {
-    return function (dispatch) {
-        return fetch(`${API_BASE_URL}/api/alerts/${phoneNumber}`, { method: 'GET', mode: 'cors', headers: { Accept: 'application/json' } })
-            .then(res => res.json())
-            .then((json) => {
-                const alerts = [];
-                json.forEach((alert) => {
-                    if (alert.alert.removeFlag === false) {
-                        alerts.push(alert);
-                    }
-                });
-                if (alerts.length === 0) {
-                    dispatch(fetchAlertEmpty());
-                } else {
-                    dispatch(fetchAlertSuccess(alerts));
-                }
-            });
-    };
+export const fetchAlert = phoneNumber => dispatch => {
+    return fetch(`${API_BASE_URL}/api/alerts/${phoneNumber}`, { 
+        method: 'GET', 
+        mode: 'cors', 
+        headers: { Accept: 'application/json' } 
+    })
+    .then(res => res.json())
+    .then(json => {
+        const alerts = [];
+        json.forEach((alert) => {
+            if (alert.alert.removeFlag === false) {
+                alerts.push(alert);
+            }
+        });
+        if (alerts.length === 0) {
+            dispatch(fetchAlertEmpty());
+        } else {
+            dispatch(fetchAlertSuccess(alerts));
+        }
+    });
 }
