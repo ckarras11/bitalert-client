2376f2582a725d2069a70f3747d44266f39be165
Christopher Karras <ckarras11@gmail.com>
2018-01-04 16:11:03 -0500

    Changed submit handler to handle creating and getting email alerts

--- src/components/modal-content.js
+++ src/components/modal-content.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchAlert, createAlert } from '../actions';
-import Horizontal from './range-slider';
+import { fetchPhoneAlert, fetchEmailAlert, createPhoneAlert, createEmailAlert } from '../actions';
 import AlertContainer from './alert-container';
 import Submit from './submit-button';
 import AlertForm from './alert-form';
-import { renderField } from './alert-form';
 function mapStateToProps(state) {
     return {
         modalTitle: state.reducer.modalTitle,
         modalType: state.reducer.modalType,
         phoneNumber: state.reducer.phoneNumber,
+        email: state.reducer.email,
         alertPrice: state.reducer.alertPrice,
         contactType: state.reducer.contactType,
 
@@ -25,41 +24,46 @@ export class ModalContent extends React.Component {
     }
 
     submitEditHandler() {
-        if(this.props.contactType === 'phoneNumber') {
+        if (this.props.contactType === 'phoneNumber') {
             const number = this.props.phoneNumber.replace(/\D/g, '');
             if (number === '') {
                 alert('Please enter a valid phone number');
             } else {
-                this.props.dispatch(fetchAlert(number));
+                this.props.dispatch(fetchPhoneAlert(number));
             }
         } else {
-            console.log('not a phone number')
+            //add email validation
+            const email = this.props.email;
+            this.props.dispatch(fetchEmailAlert(email));
+
         }
     }
     submitCreateHandler() {
-        if(this.props.contactType === 'phoneNumber') {
+        if (this.props.contactType === 'phoneNumber') {
             const number = this.props.phoneNumber.replace(/\D/g, '');
             if (number === '') {
                 alert('Please enter a valid phone number');
             } else {
-                this.props.dispatch(createAlert(number, this.props.alertPrice));
+                this.props.dispatch(createPhoneAlert(number, this.props.alertPrice));
             }
         } else {
-            console.log('not a phoneNumber')
+            //add email validation
+            const email = this.props.email;
+            this.props.dispatch(createEmailAlert(email, this.props.alertPrice));
         }
     }
 
     render() {
         let modalContent;
         if (this.props.modalType === 'info') {
-            modalContent =  <div className='width-100'>
-                                <Submit action={this.submitCreateHandler} />
-                            </div>;
+            modalContent = <div className='width-100'>
+                <Submit action={this.submitCreateHandler} />
+            </div>;
         } else if (this.props.modalType === 'danger') {
-            modalContent =  <div className='width-100'>
-                                <AlertContainer />
-                                <Submit action={this.submitEditHandler} />
-                            </div>;
+            modalContent = <div className='width-100'>
+                <AlertContainer />
+                <Submit action={this.submitEditHandler} />
+            </div>;
         }
         return (
             <div className='modal-content'>
